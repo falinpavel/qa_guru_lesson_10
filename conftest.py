21724de260e97b17c020a20b9f4179da503127d8
Falin Pavel <falinpavel96@gmail.com>
2025-08-13 12:52:52 +0300

    test yaml 3

--- conftest.py
+++ conftest.py
@@ -8,11 +8,11 @@ from selenium import webdriver
 def browser_options():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
-    # driver_options.add_argument('--window-size=1920,1080')
-    driver_options.add_argument("--headless")  # для CI
+    driver_options.add_argument('--window-size=1920,1080')
+    # driver_options.add_argument("--headless")  # для CI
     driver_options.add_argument("--no-sandbox")
-    driver_options.add_argument("--disable-dev-shm-usage")
-    driver_options.add_argument("--disable-gpu")
+    # driver_options.add_argument("--disable-dev-shm-usage")
+    # driver_options.add_argument("--disable-gpu")
     return driver_options
 
 
